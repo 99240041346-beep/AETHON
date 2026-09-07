7b6d3618c11b395da733f1e9f057651ac29aef76
99240041346-beep <99240041346@klu.ac.in>
2026-09-07 12:39:09 +0530

    feat(17m): export intelligent routing contract

---
+++ services/api/aethon/model_routing.py
@@ -0,0 +1,3 @@
+from app.model_routing import IntelligentModelRouter, ModelProfile, RoutingDecision, RoutingTelemetry, TaskComplexity
+
+__all__ = ["IntelligentModelRouter", "ModelProfile", "RoutingDecision", "RoutingTelemetry", "TaskComplexity"]
