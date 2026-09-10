b424f3b312de613a9b70ca736f4789a7df3890be
99240041346-beep <99240041346@klu.ac.in>
2026-09-10 18:42:13 +0530

    feat(30): add frontier assistant router facade

---
+++ services/api/app/frontier_assistant.py
@@ -0,0 +1,3 @@
+from aethon.assistant_control_plane import AssistantMode, AssistantRequest, AssistantResponse, capabilities, respond, router
+
+__all__ = ["AssistantMode", "AssistantRequest", "AssistantResponse", "capabilities", "respond", "router"]
